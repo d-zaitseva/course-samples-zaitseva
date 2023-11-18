22e6dc288eff7636532d86ff88f475d10d9b45f0
daria <d.d.zaitseva@gmail.com>
2023-11-18 18:44:20 +0200

    DataType feat : added models and methods

---
+++ Samples/Application/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace Models
+{
+    public class Client : Person
+    {
+        public string Phone { get; set; }
+        public int Age { get; set; }
+
+        public Client() : base() { }
+        public Client(string firstName, string lastName) : base(firstName, lastName) { }
+
+        public Client(string firstName, string lastName, string phone, int age) : base(firstName, lastName)
+        {
+            Phone = phone;
+            Age = age;
+        }
+    }
+}
---
+++ Samples/Application/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Models
+{
+    public class Person
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Person(string firstName, string lastName)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void PrintPersonData()
+        {
+            Console.WriteLine($"Firstname: {this.FirstName}");
+            Console.WriteLine($"Lastname: {this.LastName}");
+        }
+    }
+}
